8aee9f8fbd81c7d274bb06fbf424c1bf0b527023
cheungatm <cheung.atm@gmail.com>
2019-04-03 16:20:50 -0400

    This commit modifies the main_comut_script.R to allow it to be run from the command line if given the proper 9 input parameters. It also moves the example co-mut plot into an images directory for a cleaner repo.

--- main_comut_script.R
+++ main_comut_script.R
@@ -4,6 +4,23 @@
 #'               script), creates each subplot, and uses the 'egg' package to align and plot the figures together.
 #' 
 
+args <- commandArgs(trailingOnly = TRUE)
+maf_with_pair_id_path <- args[1]
+snv_gene_list_path <- args[2]
+cna_focality_file_path <- args[3]
+cna_gene_list_path <- args[4]
+melted_clinical_response_df_path <- args[5]
+melted_genomics_df_path <- args[6]
+pair_order_list_path <- args[7]
+gene_order_list_path <- args[8]
+output_file_path <- args[9]
+
+if (length(args) < 9){
+  stop("error: nine parameters must be supplied to use this function in the command line.", call.=FALSE)
+} 
+
+
+
 ### Load packages needed to run the script ###
 library(dplyr); library(tidyr); library(ggplot2); library(readr); library(egg)
 
@@ -18,15 +35,31 @@ source('./scripts/plot_CNA_bar.R')
 source('./scripts/plot_purity_tile.R')
 source('./scripts/plot_TMB_bar.R')
 
+
 ### Load files from subdirectory '.test_data/' ###
-maf_with_pair_id = read_tsv('./test_data/shuffled_maf_with_pair_id.maf', comment = '#')
-snv_gene_list = read_tsv('./test_data/shuffled_snv_gene_list.tsv')
-cna_gene_list = read_tsv('./test_data/shuffled_cna_gene_list.tsv')
-cna_focality_file = read_tsv('./test_data/shuffled_focality_file.tsv')
-melted_clinical_response_df = read_tsv('./test_data/shuffled_melted_clinical_response.tsv')
-melted_genomics_df = read_tsv('./test_data/shuffled_melted_genomics.tsv')
-pair_order_list = read_tsv('./test_data/shuffled_pair_order.tsv')
-gene_order_list = read_tsv('./test_data/shuffled_gene_order.tsv')
+#maf_with_pair_id = read_tsv('./test_data/shuffled_maf_with_pair_id.maf', comment = '#')
+maf_with_pair_id = read_tsv(maf_with_pair_id_path, comment = '#')
+
+#snv_gene_list = read_tsv('./test_data/shuffled_snv_gene_list.tsv')
+snv_gene_list = read_tsv(snv_gene_list_path)
+
+#cna_gene_list = read_tsv('./test_data/shuffled_cna_gene_list.tsv')
+cna_gene_list = read_tsv(cna_gene_list_path)
+
+#cna_focality_file = read_tsv('./test_data/shuffled_focality_file.tsv')
+cna_focality_file = read_tsv(cna_focality_file_path)
+
+#melted_clinical_response_df = read_tsv('./test_data/shuffled_melted_clinical_response.tsv')
+melted_clinical_response_df = read_tsv(melted_clinical_response_df_path)
+
+#melted_genomics_df = read_tsv('./test_data/shuffled_melted_genomics.tsv')
+melted_genomics_df = read_tsv(melted_genomics_df_path)
+
+#pair_order_list = read_tsv('./test_data/shuffled_pair_order.tsv')
+pair_order_list = read_tsv(pair_order_list_path)
+
+#gene_order_list = read_tsv('./test_data/shuffled_gene_order.tsv')
+gene_order_list = read_tsv(gene_order_list_path)
 
 
 ### Run functions to build each sub-plot ###
@@ -54,5 +87,7 @@ test_purity_plot <- plot_purity_tile(melted_data = melted_genomics_df, pair_orde
   theme(plot.margin = margin(-5,0,-5,0))
 
 ### Compile individual plots into one panel figure (order of plots matters!) ###
-egg::ggarrange(plots = list(test_TMB_plot, test_comut_plot, test_purity_plot, test_clinical_plot), ncol = 1, heights = c(1,4,1,1))
+final_panel_plot <- egg::ggarrange(plots = list(test_TMB_plot, test_comut_plot, test_purity_plot, test_clinical_plot), ncol = 1, heights = c(1,4,1,1))
 
+###
+ggsave('output_pretty_comut_plot.pdf', plot = final_panel_plot, device = "pdf", path = output_file_path, width = 26, height = 13, units = "cm", dpi = 600)
